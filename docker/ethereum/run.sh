8e3f978b6161c342e5cd8048774c4f639cf76a6d
Jaz Gulati <jaskaran.g15@gmail.com>
2020-08-17 09:45:26 +1000

    remove cloudflare flag from ganache-cli command

--- docker/ethereum/run.sh
+++ docker/ethereum/run.sh
@@ -2,9 +2,8 @@
 MNEMONIC=$1
 ADDRESS=$2
 
-ganache-cli                     \
-  -h 0.0.0.0                    \
-  -f https://cloudflare-eth.com \
-  -i 420                        \
-  -m $MNEMONIC                  \
+ganache-cli      \
+  -h 0.0.0.0     \
+  -i 420         \
+  -m "$MNEMONIC" \
   -u $ADDRESS
